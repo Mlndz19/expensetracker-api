e954c9d8bf2c57272e8fb229910ef1f3f1207826
Mlndz19 <cesar.melendezp96.dev@gmail.com>
2025-04-03 09:10:13 -0600

    refactor(models): hide password field from JSON response

--- models/user_model.go
+++ models/user_model.go
@@ -9,6 +9,6 @@ type User struct {
 	Guid     string    `json:"guid" gorm:"unique;not null"`
 	Email    string    `json:"email" binding:"required,email" gorm:"unique;not null"`
 	Username string    `json:"username" binding:"required,min=5,max=25" gorm:"unique;not null"`
-	Password string    `json:"password" binding:"required" gorm:"not null"`
+	Password string    `json:"-" binding:"required" gorm:"not null"`
 	Active   bool      `json:"active" gorm:"default:true"`
 }
