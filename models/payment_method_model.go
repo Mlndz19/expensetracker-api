cc1e2f518ee6195cc5f13a5b441df06724b566ce
Mlndz19 <cesar.melendezp96.dev@gmail.com>
2025-04-10 10:59:47 -0600

    feat(models): add constants for payment methods in PaymentMethod model

--- models/payment_method_model.go
+++ models/payment_method_model.go
@@ -6,3 +6,9 @@ type PaymentMethod struct {
 	gorm.Model
 	Description string `json:"description" binding:"required" gorm:"unique;not null"`
 }
+
+const (
+	PaymentMethodCash = "Efectivo"
+	PaymentMethodDebit = "Tarjeta de débito"
+	PaymentMethodCredit = "Tarjeta de crédito"
+)
