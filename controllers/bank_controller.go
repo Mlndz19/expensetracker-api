4827bdeb75172875b4fba02da51c35127e9d72a0
Mlndz19 <cesar.melendezp96.dev@gmail.com>
2025-04-09 22:10:12 -0600

    feat(controller): implement bank management functions including GetAllBanks, GetBankById, and CreateBank

---
+++ controllers/bank_controller.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"expensetrack/main.go/config"
+	"expensetrack/main.go/models"
+	"net/http"
+	"strconv"
+
+	"gorm.io/gorm"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetAllBanks(c *gin.Context){
+	var banks []models.Bank
+
+	if err := config.DB.Find(&banks).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"message": err})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"banks": banks})
+}
+
+func GetBankById(c *gin.Context){
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+
+	var bank models.Bank
+	if err := config.DB.First(&bank, id).Error; err != nil{
+		if err == gorm.ErrRecordNotFound {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Bank not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"bank": bank})
+}
+
+func CreateBank(c *gin.Context){
+	var bank models.Bank
+
+	if err := c.ShouldBindJSON(&bank); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := config.DB.Create(&bank).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"message": "Bank created correctly",
+		"bank": bank,
+	})
+
+}
