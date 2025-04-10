960ca890a891fd7711eed2d26c394391a8a66f7d
Mlndz19 <cesar.melendezp96.dev@gmail.com>
2025-04-10 10:59:29 -0600

    feat(controller): implement UpdateBankById and DeleteBankById functions for bank management

--- controllers/bank_controller.go
+++ controllers/bank_controller.go
@@ -63,5 +63,74 @@ func CreateBank(c *gin.Context){
 		"message": "Bank created correctly",
 		"bank": bank,
 	})
+}
 
+func UpdateBankById(c *gin.Context){
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var bank models.Bank
+	if err := config.DB.First(&bank, id).Error; err != nil{
+		if err == gorm.ErrRecordNotFound{
+			c.JSON(http.StatusNotFound, gin.H{"error": "Bank not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	var input struct{
+		Name string `json:"name" binding:"required"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := config.DB.Model(&bank).Updates(models.Bank{Name: input.Name}).Error; err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	bank.Name = input.Name
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Bank updated correctly",
+		"bank": bank,
+	})
+}
+
+func DeleteBankById(c *gin.Context){
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		return
+	}
+
+	var bank models.Bank
+	if err := config.DB.First(&bank, id).Error; err != nil{
+		if err == gorm.ErrRecordNotFound{
+			c.JSON(http.StatusNotFound, gin.H{"error": "Bank not found"})
+			return
+		}
+
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := config.DB.Delete(&bank).Error; err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "Bank deleted correctly"})
 }
